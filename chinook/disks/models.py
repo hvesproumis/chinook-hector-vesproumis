73d61fdf6a704164c35419254e55089bd8d0fb28
Hector Vesproumis <hector.vesproumis@student.isae-supaero.fr>
2024-04-12 02:23:35 +0200

    ajout app disks settings et autoriser attributs nuls

--- chinook/disks/models.py
+++ chinook/disks/models.py
@@ -12,11 +12,11 @@ class Album(models.Model):
         return self.title
     
 class Track(models.Model):
-    name = models.CharField(max_length=150)
-    composer = models.CharField(max_length=100)
+    name = models.CharField(max_length=150, null=True)
+    composer = models.CharField(max_length=100, null=True)
     milliseconds = models.BigIntegerField(default=0)
-    bytes = models.DecimalField(default=0.)
-    unitPrice = models.DecimalField(default=0.)
+    bytes = models.IntegerField(default=0)
+    unitPrice = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
