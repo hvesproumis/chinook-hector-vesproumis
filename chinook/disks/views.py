f20c835ad9b072b42b9a6e19cb5a07dc3a65fad4
Hector Vesproumis <hector.vesproumis@student.isae-supaero.fr>
2024-04-12 12:52:45 +0200

    Insertion URLs et Templates. Serveur OK.

--- chinook/disks/views.py
+++ chinook/disks/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Track
 
-# Create your views here.
+def album_list(request):
+    albums = Album.objects.all()
+    return render(request, 'disks/album_list.html', {'albums': albums})
+
+def album_detail(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    tracks = Track.objects.filter(album=album)
+    return render(request, 'disks/album_detail.html', {'album': album, 'tracks': tracks})
